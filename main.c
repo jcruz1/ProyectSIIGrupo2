fd2df1f6c58970fdebae4f8c10f6f69a922ecd32
Joe <joe.endor@hotmail.com>
2014-10-24 10:31:34 -0500

    Create main.c

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    int a,b; 
+	printf ("Bienvenido... Ingrese 2 numeros!");
+    printf ("Primer numero: ");   
+	scanf ("%d",&a); 
+    printf ("Segundo numero: ");  
+	scanf ("%d",&b); 
+	
+    //implementar resta, multiplicacion, division
+   
+    
+    return (EXIT_SUCCESS);
+}
